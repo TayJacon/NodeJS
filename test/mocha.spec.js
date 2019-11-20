c90ec87c22e2229e7fc60e630b26c986c623cf29
Taynara Jacon <tjacon@partnerfusion.com>
2019-11-20 11:40:05 -0300

    last step

--- test/mocha.spec.js
+++ test/mocha.spec.js
@@ -1,23 +1,25 @@
-var chai = require("chai");
-var chaiHttp = require("chai-http");
+import chai from 'chai';
+import chaiHttp from 'chai-http'
 
 chai.use(chaiHttp);
 
 const app = require("../app");
 const request = chai.request(app);
-//const request = chai.request("http://localhost:3000")
+// const request = chai.request("http://localhost:3000")
 
 const expect = chai.expect;
 
-describe("suite", function(){
+describe("suite", () =>{
 
-    it("meu primeiro teste", function(){
+    it("meu primeiro teste", () => {
         expect(1).to.equals(1);
         console.log("meu primeiro teste.");
     })
 
-    it("deve retornar mensagem olá", function(done) {
-        request.get("/hello").end(function(err, res){
+    it("deve retornar mensagem olá", (done) => {
+        request
+        .get("/hello")
+        .end(function(err, res){
             expect(res.body.message).to.equals("Olá, Nodejs com express!");
             done();
         })
